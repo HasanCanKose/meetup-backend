c9894682f30163561b806036d28433fe2c6b8292
Hasan Can Kose <hasancannkosee@gmail.com>
2021-01-03 20:56:03 +0300

    Add get request to get user

--- src/main/java/com/springboot/meetup/controller/UserController.java
+++ src/main/java/com/springboot/meetup/controller/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 public class UserController {
 
@@ -18,6 +20,11 @@ public class UserController {
     @RequestMapping(method = RequestMethod.POST, value = "/users/create")
     public User createUser(@RequestBody User user){
         return userService.createUser(user);
+    }
+
+    @RequestMapping("/users/get")
+    public User getUser(HttpServletRequest request) {
+        return userService.getUser(request);
     }
 
 }
--- src/main/java/com/springboot/meetup/service/UserService.java
+++ src/main/java/com/springboot/meetup/service/UserService.java
@@ -5,6 +5,9 @@ import com.springboot.meetup.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
+
 @Service
 public class UserService {
 
@@ -13,5 +16,11 @@ public class UserService {
 
     public User createUser(User user){
         return userRepository.save(user);
+    }
+
+    public User getUser(HttpServletRequest request){
+        Principal principal = request.getUserPrincipal();
+        User user = userRepository.findByUserName(principal.getName());
+        return user;
     }
 }
