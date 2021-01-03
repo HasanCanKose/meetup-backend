f2440a93b21e09d88054f29e6488461780a50601
Hasan Can Kose <hasancannkosee@gmail.com>
2021-01-03 20:44:46 +0300

    Add custom user details service

---
+++ src/main/java/com/springboot/meetup/service/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.springboot.meetup.service;
+
+import com.springboot.meetup.entity.User;
+import com.springboot.meetup.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username);
+        return new org.springframework.security.core.userdetails.User(user.getUserName(), user.getPassword(), new ArrayList<>());
+    }
+
+}
