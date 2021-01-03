a58d6462ab36b95300f85e1ee3b6b41219c3a562
Hasan Can Kose <hasancannkosee@gmail.com>
2021-01-03 20:57:31 +0300

    Fix Cors policy problem with web mvc configurer

--- src/main/java/com/springboot/meetup/MeetupApplication.java
+++ src/main/java/com/springboot/meetup/MeetupApplication.java
@@ -2,9 +2,29 @@ package com.springboot.meetup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MeetupApplication {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedHeaders("*")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("*")
+                        .allowedHeaders("Authorization", "Content-Type", "Access-Control-Allow-Origin")
+                        .exposedHeaders("Access-Control-Allow-Origin", "Authorization", "Access-Control-Allow-Headers")
+                        .maxAge(-1)
+                        .allowCredentials(true);
+            }
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MeetupApplication.class, args);
