464e8bdc1d76df362eea5cc23bf30eba2eace536
Hasan Can Kose <hasancannkosee@gmail.com>
2021-01-03 21:12:35 +0300

    Add patch request to update an event

--- src/main/java/com/springboot/meetup/controller/EventController.java
+++ src/main/java/com/springboot/meetup/controller/EventController.java
@@ -1,5 +1,6 @@
 package com.springboot.meetup.controller;
 
+import com.springboot.meetup.dto.EventUpdateDto;
 import com.springboot.meetup.entity.Event;
 import com.springboot.meetup.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,5 +34,10 @@ public class EventController {
     @RequestMapping(method = RequestMethod.POST, value = "/events")
     public Event createEvent(HttpServletRequest request, @RequestBody Event event){
         return eventService.createEvent(request, event);
+    }
+
+    @RequestMapping(method = RequestMethod.PATCH, value = "/events")
+    public Event updateEvent(@RequestBody EventUpdateDto updateDto) {
+        return eventService.updateEvent(updateDto);
     }
 }
--- src/main/java/com/springboot/meetup/service/EventService.java
+++ src/main/java/com/springboot/meetup/service/EventService.java
@@ -1,5 +1,6 @@
 package com.springboot.meetup.service;
 
+import com.springboot.meetup.dto.EventUpdateDto;
 import com.springboot.meetup.entity.Event;
 import com.springboot.meetup.entity.User;
 import com.springboot.meetup.repository.EventRepository;
@@ -7,6 +8,7 @@ import com.springboot.meetup.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
 import java.util.ArrayList;
@@ -39,6 +41,23 @@ public class EventService {
         Principal principal = request.getUserPrincipal();
         User user = userRepository.findByUserName(principal.getName());
         event.setUser(user);
+        return eventRepository.save(event);
+    }
+
+    public Event updateEvent(EventUpdateDto updateDto){
+        Event event = eventRepository.findById(updateDto.getId()).orElseThrow(() -> new EntityNotFoundException(updateDto.toString()));
+        if(updateDto.getTitle() != null){
+            event.setTitle(updateDto.getTitle());
+        }
+        if(updateDto.getDescription() != null){
+            event.setDescription(updateDto.getDescription());
+        }
+        if(updateDto.getPlace() != null){
+            event.setPlace(updateDto.getPlace());
+        }
+        if(updateDto.getDate() != null){
+            event.setDate(updateDto.getDate());
+        }
         return eventRepository.save(event);
     }
 }
