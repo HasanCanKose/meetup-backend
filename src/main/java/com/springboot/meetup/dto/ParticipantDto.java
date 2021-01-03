d4d5cf4b2cc85b154a6257dc2d6511f4e83e29c9
Hasan Can Kose <hasancannkosee@gmail.com>
2021-01-03 20:41:13 +0300

    Add participant dto

---
+++ src/main/java/com/springboot/meetup/dto/ParticipantDto.java
@@ -0,0 +1,15 @@
+package com.springboot.meetup.dto;
+
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ParticipantDto {
+
+    private int eventId;
+
+
+}
